11ddaea6c5a83e7a0a4095a501f04d525ed1ed43
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-11-12 11:20:15 -0500

    Promises 02

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -1,3 +1,28 @@
 import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1';
 
+function fetchData(urlApi) {
+    return fetch(urlApi);
+}
+
+// fetchData(`${API}/products`)
+//     .then(response => response.json())
+//     .then(products => console.log(products[0]))
+//     .then(() => console.log('hola'))
+//     .catch(error => console.error(error));
+
+fetchData(`${API}/products`)
+    .then(response => response.json())
+    .then(products => {
+        console.log(products[0]);
+        return fetchData(`${API}/products/${products[0]?.id}`)
+    })
+    .then(response => response.json())
+    .then(product => {
+        console.log(`Product Title: ${product.title}`);
+        return fetchData(`${API}/categories/${product?.category.id}`)
+    })
+    .then(response => response.json())
+    .then(category => console.log(`Category Name: ${category.name}`))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finally!'));
