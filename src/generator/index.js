69e40053ce2de2f8b0c112c0ee6ea995cfb29b69
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-11-14 11:47:47 -0500

    Generator 01

---
+++ src/generator/index.js
@@ -0,0 +1,24 @@
+function* generatorFn() {
+    yield 1;
+    yield 2;
+    yield 3;
+};
+
+const g = generatorFn();
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+
+function* iterate(array) {
+    for (let value of array) {
+        yield value;
+    }
+}
+
+const it = iterate(['oscar', 'omar', 'ana', 'lucia', 'juan']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().done);
